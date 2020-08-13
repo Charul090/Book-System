ec88a7c8e1d76feaac0666eaf67a260bfc4da537
Charul090 <baricharul9@hotmail.com>
2020-08-13 15:33:20 +0530

    Added login service

--- server/app/services/user.py
+++ server/app/services/user.py
@@ -1,6 +1,8 @@
 import jwt
 import json
 from ..models import db,UsersModel
+import datetime
+from instance.config import secret
 
 
 def registerUser(data):
@@ -20,3 +22,25 @@ def registerUser(data):
         return json.dumps({"error":True,"message":"Email already exists"})
 
     return "Success"
+
+def loginUser(data):
+    email = data["email"]
+    password = data["password"]
+
+    status = UsersModel.query.filter(UsersModel.email == email).first()
+
+    if status is None:
+        return json.dumps({"error":True,"message":"Email doesn't exist"})
+    else:
+        if status.password == password:
+            obj={
+                "email":email,
+                "created":str(datetime.datetime.utcnow()),
+                "expiry":str(datetime.datetime.utcnow()+datetime.timedelta(days=1))
+            }
+
+            token = jwt.encode(obj,secret)
+
+            return json.dumps({"error":False,"token":token.decode(),"message":"Login Successfull"})
+        else:
+            return json.dumps({"error":True,"message":"Wrong Password"})
