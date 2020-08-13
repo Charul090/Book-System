9459d2e6f864c895a9f995fc0f978f543eaf568d
Charul090 <baricharul9@hotmail.com>
2020-08-13 16:13:04 +0530

    Added add book service

--- server/app/services/book.py
+++ server/app/services/book.py
@@ -1,4 +1,46 @@
+import json
+from ..models import db, BookModel, UsersModel, CategoryModel
+from ..util.token import decodeToken
 
 
-def getBook(data):
-    pass
+def getBook(page):
+    data = BookModel.query.all()
+
+    
+
+
+def addBook(data, token):
+    token = decodeToken(token)
+
+    if token is False:
+        return json.dumps({"message": "Token Expired", "error": True})
+
+    name = data["name"]
+    price = data["price"]
+    description = data["description"]
+    quantity = data["quantity"]
+
+    user = UsersModel.query.filter(UsersModel.email == token["email"]).first()
+    user_id = user.id
+
+    category_id = CategoryModel.query.filter(
+        CategoryModel.category == data["category"]).first()
+    category_id = category_id.id
+
+    book = BookModel(name=name, price=price, description=description,
+                     quantity=quantity, user_id=user_id, category=category_id)
+
+    db.session.add(book)
+    db.session.commit()
+
+    return json.dumps({"error": False, "message": "Book Added Successfully"})
+
+
+def sendCategory():
+    category = CategoryModel.query.all()
+    data = []
+
+    for x in category:
+        data.append(x.category)
+
+    return json.dumps({"error": False, "category": data})
