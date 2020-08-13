0f7abfad8e987c5dc07ccb60f030d1ba510af7b5
Charul090 <baricharul9@hotmail.com>
2020-08-13 15:33:05 +0530

    Added login route

--- server/app/routes/User.py
+++ server/app/routes/User.py
@@ -1,9 +1,15 @@
 from . import user
-from ..services.user import registerUser
+from ..services.user import registerUser,loginUser
 from flask import request
 
 
 @user.route("/register", methods=["POST"])
 def register():
     response = registerUser(request.json)
+    return response
+
+
+@user.route("/login", methods=["POST"])
+def login():
+    response = loginUser(request.json)
     return response
