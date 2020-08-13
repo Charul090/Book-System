46351bb771ffe6ef73a798a820c2f7bccdf05d16
Charul090 <baricharul9@hotmail.com>
2020-08-13 16:26:51 +0530

    Added model fix

--- server/app/models/BookModel.py
+++ server/app/models/BookModel.py
@@ -1,11 +1,13 @@
 from . import db
 
+
 class BookModel(db.Model):
     __tablename__ = "book"
-    id = db.Column(db.Integer,primary_key=True)
+    id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(100))
     price = db.Column(db.Integer)
-    category = db.Column(db.Integer,db.ForeignKey("category.id"))
+    category = db.Column(db.Integer, db.ForeignKey("category.id"))
     description = db.Column(db.String(1000))
     quantity = db.Column(db.Integer)
-    user_id = db.Column(db.Integer,db.ForeignKey("user.id"))
+    user_id = db.Column(db.Integer, db.ForeignKey("user.id"))
+    author = db.Column(db.String(100))
--- server/app/models/CategoryModel.py
+++ server/app/models/CategoryModel.py
@@ -3,5 +3,5 @@ from . import db
 class CategoryModel(db.Model):
     __tablename__ = "category"
     id = db.Column(db.Integer,primary_key=True)
-    category = db.Column(db.String(100))
+    category = db.Column(db.String(100),unique=True)
 
